b3c423122766ede73cf2df063a1e5c5611c4ca57
Killian KOPP <killiankopp@gmail.com>
2025-02-27 15:09:18 +0100

    Update create_secrets.sh

--- create_secrets.sh
+++ create_secrets.sh
@@ -2,4 +2,5 @@ kubectl create secret generic keycloak-secret \
   --from-literal=KEYCLOAK_HOST="https://www.urlserveur.bzh" \
   --from-literal=KEYCLOAK_REALM="myrealm" \
   --from-literal=KEYCLOAK_CLIENT_ID="myid" \
-  --from-literal=KEYCLOAK_CLIENT_SECRET="mypassword"
+  --from-literal=KEYCLOAK_CLIENT_SECRET="mypassword" \
+  -n karned
